7c2b4578a40105624885224c8e4eea8d25f3aefc
BixbyRamone <gregorytdraheim@gmail.com>
2020-03-11 14:53:03 -0400

    second commit

---
+++ src/app/Tabs/about-me/volunteer-work/volunteer-work.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-volunteer-work',
+  templateUrl: './volunteer-work.component.html',
+  styleUrls: ['./volunteer-work.component.css']
+})
+export class VolunteerWorkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,27 @@ import { TabbedPanelComponent } from './tabbed-panel/tabbed-panel.component';
 import { SkillsComponent } from './skills/skills.component';
 import { HomeComponent } from './home/home.component';
 import { PagesComponent } from './pages/pages.component';
+import { AboutMeComponent } from './Tabs/about-me/about-me.component';
+import { ProjectsComponent } from './Tabs/about-me/projects/projects.component';
+import { EducationComponent } from './Tabs/about-me/education/education.component';
+import { WorkHistoryComponent } from './Tabs/about-me/work-history/work-history.component';
+import { VolunteerWorkComponent } from './Tabs/about-me/volunteer-work/volunteer-work.component';
+import { ContactComponent } from './Tabs/about-me/contact/contact.component';
 
 @NgModule({
    declarations: [
+      AboutMeComponent,
       AppComponent,
-      NavbarComponent,
-      TabbedPanelComponent,
-      SkillsComponent,
+      ContactComponent,
+      EducationComponent,
       HomeComponent,
-      PagesComponent
+      NavbarComponent,
+      PagesComponent,
+      ProjectsComponent,
+      SkillsComponent,
+      TabbedPanelComponent,
+      VolunteerWorkComponent,
+      WorkHistoryComponent
    ],
    imports: [
       BrowserModule,
